fc15cffbb5f95647a3f65187b60911ffebc713ae
Thien-Nam Dinh <thiennam.tnd@gmail.com>
2018-03-04 02:55:35 -0600

    changed the default value on MoneTorPriorityMod to 1.0

--- src/or/config.c
+++ src/or/config.c
@@ -214,7 +214,7 @@ static config_var_t option_vars_[] = {
   V(MoneTorPublicMint,           BOOL,     "0"),
   V(MoneTorSingleThread,         BOOL,     "0"),
   V(MoneTorSingleCore,           BOOL,     "0"),
-  V(MoneTorPriorityMod,          DOUBLE,   "0.0"),
+  V(MoneTorPriorityMod,          DOUBLE,   "1.0"),
   V(MoneTorInitialWindow,        INT,     "3000"),
   V(MoneTorPaymentRate,          INT,     "2000"),
   V(AccountingMax,               MEMUNIT,  "0 bytes"),
